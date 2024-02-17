8f62095d0c7b402454107578fc099881a8f8447d
PoojaKrishna28 <68842189+PoojaKrishna28@users.noreply.github.com>
2024-02-17 10:30:13 +0530

    Create variables.tf

---
+++ challenge#1/variables.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "This is variable for location"  
+  type = string
+}
+
+variable "subscription_id" {
+    description = "Subscription to deploy resources into"
+    type = string 
+}
